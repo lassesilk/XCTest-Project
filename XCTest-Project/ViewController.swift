da0612e3396db238c9945f5234021af71748951a
Lasse N Silkoset <lassesilkoset@gmail.com>
2020-05-24 22:19:01 +0200

    Initial Commit

---
+++ XCTest-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCTest-Project
+//
+//  Created by Lasse Silkoset on 24/05/2020.
+//  Copyright © 2020 Lasse Silkoset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XCTest-ProjectTests/XCTest_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  XCTest_ProjectTests.swift
+//  XCTest-ProjectTests
+//
+//  Created by Lasse Silkoset on 24/05/2020.
+//  Copyright © 2020 Lasse Silkoset. All rights reserved.
+//
+
+import XCTest
+@testable import XCTest_Project
+
+class XCTest_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
